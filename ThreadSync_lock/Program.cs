fbb069e2a5f7d723048830b0700dd487485f4d21
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-08-08 22:03:51 +1000

    Добавил пример использования lock

---
+++ ThreadSync_lock/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadSync_lock
+{
+    class Program
+    {
+        private static int x = 0;
+
+        private static object locker = new object();
+        
+        static void Main(string[] args)
+        {
+            // StartThreads_WithoutLock();
+            StartThreads_WithLock();
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Пример потоков без использования синхронизации (lock)
+        /// </summary>
+        private static void StartThreads_WithoutLock()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread thread = new Thread(Count);
+                thread.Name = $"Поток {i.ToString()}";
+                thread.Start();
+            }
+        }
+        
+        private static void Count()
+        {
+            x = 1;
+
+            for (int i = 1; i < 9; i++)
+            {
+                Console.WriteLine("{0}: {1}", Thread.CurrentThread.Name, x);
+                x++;
+                Thread.Sleep(100);
+            }
+        }
+
+        /// <summary>
+        /// Пример потоков c использованием синхронизации
+        /// </summary>
+        private static void StartThreads_WithLock()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread thread = new Thread(Count_WithLock);
+                thread.Name = $"Поток {i.ToString()}";
+                thread.Start();
+            }
+        }
+
+        private static void Count_WithLock()
+        {
+            lock (locker)
+            {
+                x = 1;
+
+                for (int i = 1; i < 9; i++)
+                {
+                    Console.WriteLine("{0}: {1}", Thread.CurrentThread.Name, x);
+                    x++;
+                    Thread.Sleep(100);
+                }
+            }
+        }
+    }
+}
