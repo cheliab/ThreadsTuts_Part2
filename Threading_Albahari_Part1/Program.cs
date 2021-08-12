22e3ab293b93323565363e9b2eb87b9941973fe4
Pavel Berezkin <Berezkin.P@dns-shop.ru>
2021-08-13 09:28:18 +1000

    Добавил супер простой пример создания потоков с выводом XY в консоль

---
+++ Threading_Albahari_Part1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Threading_Albahari_Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread_01_XY_Example.Start();
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Threading_Albahari_Part1/Thread_01_XY_Example.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading_Albahari_Part1
+{
+    /// <summary>
+    /// Простой пример нескольких потоков
+    ///
+    /// http://www.albahari.com/threading/
+    /// </summary>
+    public class Thread_01_XY_Example
+    {
+        public static void Start()
+        {
+            // В новго потоке пишем "Y" в консоль
+            Thread thread = new Thread(WriteY);
+            thread.Start();
+            
+            // В основном потоке пишем "X" в консоль
+            WriteX();
+        }
+
+        private static void WriteX()
+        {
+            for (int i = 0; i < 300; i++)
+                Console.Write("x");
+        }
+
+        private static void WriteY()
+        {
+            for (var i = 0; i < 300; i++)
+                Console.Write("y");
+        }
+    }
+}
