558c467c8c60de33b44acf936712a8491e657d5d
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-08-14 13:30:03 +1000

    Добавил примеры возможных проблем с переменными при передаче в потоки

--- Threading_Albahari_Part1/Program.cs
+++ Threading_Albahari_Part1/Program.cs
@@ -13,7 +13,10 @@ namespace Threading_Albahari_Part1
             // Thread_05_BrokeShareData.Start();
             // Thread_06_LockShareStaticData.Start();
             // Thread_07_JoinSleep.Start();
-            Thread_08_Delegate_ThreadStart.Start();
+            // Thread_08_Delegate_ThreadStart.Start();
+            // Thread_10_LambdaExp_CapturedVar.Start_forLoopExample();
+            // Thread_10_LambdaExp_CapturedVar.Start_StringExample();
+            Thread_10_LambdaExp_CapturedVar.Start_TempVarSolution();
             
             Console.ReadLine();
         }
---
+++ Threading_Albahari_Part1/Thread_10_LambdaExp_CapturedVar.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading_Albahari_Part1
+{
+    /// <summary>
+    /// Пример ошибки с использование лябда выражений
+    /// </summary>
+    public class Thread_10_LambdaExp_CapturedVar
+    {
+
+        public static void Start_forLoopExample()
+        {
+            // Проблема в том,
+            // что i переменная ссылается на одну и ту же ячейку памяти
+            // на протяжении всего цикла.
+            // Следовательно, каждый поток вызывает Console.Write переменную,
+            // значение которой может меняться во время работы!
+            
+            // Вариант вывода: 32225671089
+            // Проблема в повторах "222"
+            for (int i = 0; i < 10; i++)
+                new Thread(() => Console.Write(i)).Start();
+        }
+
+        public static void Start_TempVarSolution()
+        {
+            // При использование временной переменной все значения будут разные
+            // Так как ссылаются на разные ячейки памяти
+            // Вариант вывода: 1203456789
+            for (int i = 0; i < 10; i++)
+            {
+                int temp = i;
+                new Thread(() => Console.Write(temp)).Start();
+            }
+        }
+
+        public static void Start_StringExample()
+        {
+            string text = "t1";
+            Thread thread_1 = new Thread(() => Console.WriteLine(text));
+
+            text = "t2";
+            Thread thread_2 = new Thread(() => Console.WriteLine(text));
+            
+            thread_1.Start();
+            thread_2.Start();
+        }
+    }
+}
