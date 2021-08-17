be90d618fd1032e97dc95762de6fef13d6a78780
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-08-18 00:12:43 +1000

    Добавил пример использования BeginInvoke и EndInvoke

---
+++ Threading_Albahari_Part1/ThreadPoolExamples/ThreadPool_04_BeginInvoke_EndInvoke.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace Threading_Albahari_Part1.ThreadPoolExamples
+{
+    public class ThreadPool_04_BeginInvoke_EndInvoke
+    {
+        public static void Start()
+        {
+            Func<string, int> method = Work;
+            IAsyncResult cookie = method.BeginInvoke("test", null, null);
+
+            // тут может паралельно выполняться другая работа
+            
+            // тут происходит получение результата метода
+            // (если метод еще не завершится текущий поток блокируется)
+            int result = method.EndInvoke(cookie); 
+        }
+
+        private static int Work(string s)
+        {
+            return s.Length;
+        }
+    }
+}
