ecbf05fa8fb477e674175fded6bec8ddbf3a66d1
Pavel Berezkin <Berezkin.P@dns-shop.ru>
2021-08-20 09:24:36 +1000

    Добавил пример из пункта When to lock (когда стоит блокировать)

---
+++ Threading_Albahari_Part2/Locking_05_WhenToLock.cs
@@ -0,0 +1,47 @@
+﻿namespace Threading_Albahari_Part2
+{
+    // http://www.albahari.com/threading/part2.aspx#_When_to_Lock
+    //
+    // When to Lock
+    //
+    // Когда стоит блокировать
+    //
+    // Как правило, стоит блокировать доступ к любому общему полю, доступному для записи.
+
+    /// <summary>
+    /// Не потокобезопасный вариант
+    /// </summary>
+    class ThreadUnsafe_05
+    {
+        private static int _x;
+
+        static void Increment()
+        {
+            _x++;
+        }
+
+        static void Assing()
+        {
+            _x = 123;
+        }
+    }
+
+    /// <summary>
+    /// Потокобезопасный вариант
+    /// </summary>
+    class ThreadSafe_05
+    {
+        private static readonly object _locker = new object();
+        private static int _x;
+
+        static void Increment()
+        {
+            lock (_locker) _x++;
+        }
+        
+        static void Assing()
+        {
+            lock (_locker) _x = 123;
+        }
+    }
+}
