fee5435139be631234844d3facd873a39894f864
Pavel Berezkin <Berezkin.P@dns-shop.ru>
2021-08-23 09:46:25 +1000

    Добавил пример с мютексом (mutex)

---
+++ Threading_Albahari_Part2/Locking_09_Mutex.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading_Albahari_Part2
+{
+    // A Mutex похож на C # lock,
+    // но может работать с несколькими процессами.
+    // Другими словами, он Mutex может быть как в масштабе компьютера, так и в масштабе приложения .
+    public class Locking_09_Mutex
+    {
+        public static void Start()
+        {
+            using (var mutex = new Mutex(false, "berezki.p app - Locking_09_Mutex)"))
+            {
+                if (!mutex.WaitOne(TimeSpan.FromSeconds(1), false))
+                {
+                    Console.WriteLine("Another app instance is running. Bye!");
+                    return;
+                }
+
+                RunProgram();
+            }
+        }
+
+        private static void RunProgram()
+        {
+            Console.WriteLine("Running. Press Enter to exit.");
+        }
+    }
+}
--- Threading_Albahari_Part2/Program.cs
+++ Threading_Albahari_Part2/Program.cs
@@ -8,7 +8,9 @@ namespace Threading_Albahari_Part2
         {
             // Locking_01.Start_ThreadUnsafe();
             // Locking_01.Start_ThreadSafe();
-            Locking_02_Monitor_EnterExit.Start();
+            // Locking_02_Monitor_EnterExit.Start();
+            
+            Locking_09_Mutex.Start();
             
             Console.ReadLine();
         }
