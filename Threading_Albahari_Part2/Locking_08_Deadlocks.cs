2b7e4235a71356064630b5efdceb706bae99f8ee
Pavel Berezkin <Berezkin.P@dns-shop.ru>
2021-08-23 09:15:37 +1000

    Добавил комментов в пример с взаимоблокировкой

--- Threading_Albahari_Part2/Locking_08_Deadlocks.cs
+++ Threading_Albahari_Part2/Locking_08_Deadlocks.cs
@@ -4,14 +4,18 @@ namespace Threading_Albahari_Part2
 {
     /// <summary>
     /// Deadlock (Взаимоблокировка потоков)
+    ///
+    /// Взаимоблокировка возникает, когда каждый из двух потоков ожидает ресурса, удерживаемого другим, поэтому ни один из них не может продолжить работу.
     /// </summary>
     public class Locking_08_Deadlocks
     {
+        // В такой ситуации должно присутствовать более одного объекта блокировки
         private static object _locker1 = new object();
         private static object _locker2 = new object();
 
         public static void Start()
         {
+            // Вспомогательный поток сначала блокирует _locker1 и потом ждет _locker2
             new Thread(() =>
             {
                 lock (_locker1)
@@ -21,11 +25,14 @@ namespace Threading_Albahari_Part2
                 }
             }).Start();
 
+            // Основной поток наоборот сначала блокирует _locker2 и потом ждет _locker1
             lock (_locker2)
             {
                 Thread.Sleep(1000);
                 lock (_locker1); // Deadlock
             }
+            
+            // Теперь оба потока ожидают пока освободится объект блокировки заблокированный в другом потоке
         }
     }
 }
