9ceff64f9563a4b6cb7f38bd83aae8b13bf9dd31
Pavel Berezkin <Berezkin.P@dns-shop.ru>
2021-08-25 10:31:55 +1000

    Добавил пример дедлока с вложенными блокировками (deadlock - nested lock)

---
+++ DeadlocksInDepth_Part1/Example_02_Deadlock_NestedLocks.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DeadlocksInDepth_Part1
+{
+    public class Example_02_Deadlock_NestedLocks
+    {
+        public void Start()
+        {
+            Console.WriteLine("Starting...");
+            
+            object lock1 = new object();
+            object lock2 = new object();
+
+            var task1 = Task.Run(() =>
+            {
+                lock (lock1)
+                {
+                    Thread.Sleep(1000);
+                    lock (lock2)
+                    {
+                        Console.WriteLine("Finished Thread 1");
+                    }
+                }
+            });
+
+            var task2 = Task.Run(() =>
+            {
+                lock (lock2)
+                {
+                    Thread.Sleep(1000);
+                    lock (lock1)
+                    {
+                        Console.WriteLine("Finished Thread 2");
+                    }
+                }
+            });
+
+            Task.WaitAll(task1, task2);
+            
+            Console.WriteLine("Finished...");
+        }
+    }
+}
