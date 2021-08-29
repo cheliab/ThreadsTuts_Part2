b2fe166344eee1303b800f68fa4b4d5562435fe6
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-08-30 07:53:06 +1000

    Добавил наборосок примера с неизменяемыми объектами - (Immutable objects)

---
+++ Threading_Albahari_Part2/02/ThreadSafety_03_ImmutableObjects.cs
@@ -0,0 +1,25 @@
+﻿namespace Threading_Albahari_Part2
+{
+    public class ProgressStatus
+    {
+        public readonly int PercentComplete;
+        public readonly string StatusMessage;
+
+        public ProgressStatus(int percentComplete, string statusMessage)
+        {
+            PercentComplete = percentComplete;
+            StatusMessage = statusMessage;
+        }
+    }
+
+    public class ThreadSafety_03_ImmutableObjects
+    {
+        private readonly object _statusLocker = new object();
+        private ProgressStatus _status;
+
+        public static void Start()
+        {
+            var status = new ProgressStatus(50, "");
+        }
+    }
+}
