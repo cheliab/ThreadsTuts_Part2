33fbda3113c96d56836ede3024f821d0300045f1
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-08-31 00:01:08 +1000

    Доп. пример AutoResetEvent с ожиданием ввода

---
+++ Threading_Albahari_Part2/03_EventWaitHandles/EventWaitHandles_01_AutoResetEvent_01_WaitInput.cs
@@ -0,0 +1,14 @@
+﻿namespace Threading_Albahari_Part2._03_EventWaitHandles
+{
+    // Доп. пример с ожиданием ввода
+    //
+    // https://www.youtube.com/watch?v=xaaRBh07N34
+    
+    public class EventWaitHandles_01_AutoResetEvent_01_WaitInput
+    {
+        public static void Start()
+        {
+            
+        }
+    }
+}
