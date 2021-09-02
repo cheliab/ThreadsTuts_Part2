808eb3a5d64a1a0eab5549dbff551ef83b768c6d
Pavel Berezkin <Berezkin.P@dns-shop.ru>
2021-09-03 09:46:42 +1000

    Добавил комменты по запуску метода в пуле потоков

--- Threading_Albahari_Part1/ThreadPoolExamples/ThreadPool_01_EnteringTheThreadPool.cs
+++ Threading_Albahari_Part1/ThreadPoolExamples/ThreadPool_01_EnteringTheThreadPool.cs
@@ -3,11 +3,21 @@ using System.Threading.Tasks;
 
 namespace Threading_Albahari_Part1.ThreadPoolExamples
 {
+    /// <summary>
+    /// Вход в пул потоков используюя TPL (Task Parallel Library)
+    /// </summary>
     public class ThreadPool_01_EnteringTheThreadPool
     {
         public static void Start()
         {
+            // Чтобы использовать неуниверсальный Task класс, вызовите Task.Factory.StartNew, передав делегат целевого метода:
+            
+            // Запуск метода в потоке из пула потоков 
             Task.Factory.StartNew(Go);
+            
+            // Task.Factory.StartNew возвращает Task объект,
+            // который затем можно использовать для отслеживания задачи - например,
+            // вы можете дождаться ее завершения, вызвав его Wait() метод.
         }
 
         private static void Go()
